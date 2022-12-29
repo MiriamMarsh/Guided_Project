de24450292c52111f48ce171750563b034c5eb65
Miriam Marsh <mmarsh@integralytic.com>
2022-12-29 13:04:23 +0200

    added 2 functions catplot and boxenplot

---
+++ guided_project_functions.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("hotel_bookings.csv")
+
+def create_catplot(x):
+    sns.catplot(x=x, kind='count', data=df).set(title = 'Bar chart of {} column'.format(x))
+    return
+
+def create_boxenplot(y):
+    sns.boxenplot(x= df[y])
+    return
+
