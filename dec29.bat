2fc9ecbe41a25df0f641d99bc8d6502b1aa33878
Miriam Marsh <mmarsh@integralytic.com>
2023-01-02 12:24:56 +0200

    Finished command line work for dec29

--- dec29.bat
+++ dec29.bat
@@ -1,2 +1,17 @@
-python 
+echo off
+echo starting
+@REM calling my python file below
+python dec29.py %name% %teacher%
+set name=%1
+set teacher=%2
+@REM adding text to a new file called file.txt
+echo "Welcome to class! This is for taking notes" > file.txt
+@REM opening the new file.txt that I just created
+start file.txt
+@REM adding text to a new file called file.docx
+echo "Welcome to class! This is for taking notes" > file.docx
+@REM opening the new file.docx that I just created
+start file.docx
+@REM opening slack on my computer
 start C:\Users\Owner\AppData\Local\slack\slack.exe
+echo is completed
--- dec29.py
+++ dec29.py
@@ -1,10 +1,37 @@
 import pandas as pd
 from datetime import datetime
 import sys
-name = sys.argv[1]
-def welcome_to_class(name):
-    print("welcome to this class", name)
-    print(datetime.now())
+name=sys.argv[1]
+teacher=sys.argv[2]
+ #   def welcome_to_class(name):
+ #   print("welcome to this class", name)
+ #   print(datetime.now())
 
-welcome_to_class(name)
+# welcome_to_class(name)
+import datetime
+def classes(name, teacher):
+    d=datetime.datetime.now()
+    print("Welcome to this class,", name)
+    print(d)
+    time=d.strftime("%H:%M")
+    while True:
+        if time < ("09:00"):
+            print("before class")
+        elif time > ("09:00") and  time < ("10:50"):
+             print("during class")
+        elif time > ("10:50") and time < ("11:10"):
+             print("break time")  
+        elif time > ("11:10") and time < ("13:00"):
+             print("Class Time")
+        else:
+            print("Make sure to do your HMWK!")    
+        if teacher == "Mrs.A":
+            print ("We will be learning the letter A")
+        if teacher == "Mrs.B":
+            print ("We will be learning the letter B Today with,", teacher)
+        break
+classes(name, teacher) 
+
+    # "before class" if its before 9:00, "during class" if its during class time, and "break" if its 10:50-11:10
+   
    
